8a554e20815e2a6d74aef390a760d67549c8998c
franksymon <78883574+franksymon@users.noreply.github.com>
2022-05-24 10:56:41 -0400

    config heroku

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express');
-
-//Inint express app
-const app = express();
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const compression = require('compression');
+const morgan = require('morgan');
 
 //Routers
 const { usersRouter } = require('./routes/userRouter');
@@ -10,8 +11,30 @@ const { repairsRouter } = require('./routes/repairsRouter');
 //Controllers
 const { globalErrorHandler } = require('./controllers/errosController');
 
+//Inint express app
+const app = express();
+
 // Enable incoming JSON data
 app.use(express.json());
+
+// Add security headers
+app.use(helmet());
+
+// Compress responses
+app.use(compression());
+
+// Log incoming requests
+if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
+else app.use(morgan('combined'));
+
+// Limit IP requests
+const limiter = rateLimit({
+  max: 10000,
+  windowMs: 1 * 60 * 60 * 1000, // 1 hr
+  message: 'Too many requests from this IP',
+});
+
+app.use(limiter);
 
 //Endponits
 app.use('/api/v1/users', usersRouter);
